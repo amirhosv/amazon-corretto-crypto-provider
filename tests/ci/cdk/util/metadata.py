c3cef3c0c50954bb3054fe5e1d021ab23be18abd
Amir Vakili <amirhosv@amazon.com>
2023-02-23 17:53:48 +0000

    CI task update

--- tests/ci/cdk/util/metadata.py
+++ tests/ci/cdk/util/metadata.py
@@ -6,15 +6,15 @@
 from util.env_util import EnvUtil
 
 # Used when AWS CDK defines AWS resources.
-AWS_ACCOUNT = EnvUtil.get("CDK_DEPLOY_ACCOUNT", "838297025124")
-AWS_REGION = EnvUtil.get("CDK_DEPLOY_REGION", "us-west-2")
+AWS_ACCOUNT = EnvUtil.get("CDK_DEPLOY_ACCOUNT", "158043403898")
+AWS_REGION = EnvUtil.get("CDK_DEPLOY_REGION", "us-east-1")
 
 # Used when AWS CDK defines ECR repos.
 LINUX_ECR_REPO = EnvUtil.get("ECR_LINUX_REPO_NAME", "accp-docker-images-linux")
 WINDOWS_X86_ECR_REPO = EnvUtil.get("ECR_WINDOWS_X86_REPO_NAME", "accp-docker-images-windows-x86")
 
 # Used when AWS CodeBuild needs to create web_hooks.
-GITHUB_REPO_OWNER = EnvUtil.get("GITHUB_REPO_OWNER", "corretto")
+GITHUB_REPO_OWNER = EnvUtil.get("GITHUB_REPO_OWNER", "amirhosv")
 GITHUB_REPO_NAME = EnvUtil.get("GITHUB_REPO_NAME", "amazon-corretto-crypto-provider")
 GITHUB_SOURCE_VERSION = EnvUtil.get("GITHUB_SOURCE_VERSION", "develop")
 
